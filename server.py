d1eb96f8bd06b89e599d909d08baf7a45cd0bade
Sébastien Combéfis <seb478@gmail.com>
2016-11-20 11:13:56 +0100

    Server: Add list of links variable
    - Added an instance variable with the list of links
    - Changed index route to show the list of links

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 import cherrypy
 
 class ShareYourLinks():
+    def __init__(self):
+        self.links = self.loadlinks('db.json')
+
+    def loadlinks(self, path):
+        return []
+
     @cherrypy.expose
     def index(self):
-        return 'Welcome on ShareMyLinks!'
+        links = ''
+        for link in self.links:
+            links += '<li>{}</li>'.format(link['title'])
+        return '<h1>Welcome on ShareMyLinks!</h1><ul>{}</ul>'.format(links)
 
 if __name__ == '__main__':
     cherrypy.quickstart(ShareYourLinks())
