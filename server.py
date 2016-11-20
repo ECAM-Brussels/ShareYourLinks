1d88d672e91864626dfae2ab2ed36ea0b0835972
Sébastien Combéfis <seb478@gmail.com>
2016-11-20 12:06:12 +0100

    Server: Add form to add a new link
    - Added routes for a form to add a new link (add and addlink)
    - Updated index route to add link to the form
    - Updated index route content to show links' details

--- server.py
+++ server.py
@@ -9,10 +9,49 @@ class ShareYourLinks():
 
     @cherrypy.expose
     def index(self):
-        links = ''
-        for link in self.links:
-            links += '<li>{}</li>'.format(link['title'])
-        return '<h1>Welcome on ShareMyLinks!</h1><ul>{}</ul>'.format(links)
+        if len(self.links) == 0:
+            links = '<p>Aucun lien.</p>'
+        else:
+            links = '<ol>'
+            for link in self.links:
+                links += '<li><a href="{}">{}</a><br/><small>{}</small></li>'.format(link['link'], link['title'], link['description'])
+            links += '</ol>'
+        return '''<html>
+  <head>
+    <title>ShareMyLinks</title>
+  </head>
+  <body>
+    <h1>Welcome on ShareMyLinks!</h1>
+    {}
+    <p><a href="add">Ajouter un lien</a></p>
+  </body>
+</html>'''.format(links)
+
+    @cherrypy.expose
+    def add(self):
+        return '''<html>
+  <head>
+    <title>ShareMyLinks</title>
+  </head>
+  <body>
+    <h1>Ajouter un lien</h1>
+    <form method="post" action="addlink">
+      <p><label>Titre<br /><input type="text" name="title" /></label></p>
+      <p><label>Lien<br /><input type="text" name="link" /></label></p>
+      <p><label>Description<br /><textarea name="description" rows="5" cols="40" /></textarea></label></p>
+      <p><button type="submit">Ajouter</button></p>
+    </form>
+  </body>
+</html>'''
+
+    @cherrypy.expose
+    def addlink(self, title, link, description):
+        self.links.append({
+            'title': title,
+            'link': link,
+            'description': description
+        })
+        raise cherrypy.HTTPRedirect('/')
 
 if __name__ == '__main__':
     cherrypy.quickstart(ShareYourLinks())
